7c2f6fe1c91117c1ae12ee3d1815e2fee8458248
syluzard <144542672+syluzard@users.noreply.github.com>
2023-10-11 21:44:08 +0800

    Add files via upload

--- lecture4-2.py
+++ lecture4-2.py
@@ -26,10 +26,11 @@ def getfragments(seq, motif):
 
     #3-1
     pre_motif_in_seq_fistposition = seq.find(pre_motif_str)
-    #print("seq[pre_motif_in_seq_fistposition+1:] : ",seq[pre_motif_in_seq_fistposition+1:]);
+    #print("len(pre_motif_str): ", len(pre_motif_str))
+    #print("seq[pre_motif_in_seq_fistposition+len(pre_motif_str):] : ",seq[pre_motif_in_seq_fistposition+len(pre_motif_str):]);
 
     #3-2
-    post_motif_in_seq_fistposition  = seq[pre_motif_in_seq_fistposition+1:].find(post_motif_str)
+    post_motif_in_seq_fistposition  = seq[pre_motif_in_seq_fistposition+len(pre_motif_str):].find(post_motif_str)
     #print("pre_motif_in_seq_fistposition: ",  pre_motif_in_seq_fistposition );
     #print("post_motif_in_seq_fistposition: ", post_motif_in_seq_fistposition);
 
@@ -43,7 +44,7 @@ def getfragments(seq, motif):
     elif pre_motif_in_seq_fistposition < 0 :
         exist = False;
         return exist, frag1, frag2;
-    elif pre_motif_in_seq_fistposition + post_motif_in_seq_fistposition + 2 + len(post_motif_str) == len(seq):
+    elif pre_motif_in_seq_fistposition + len(pre_motif_str) + post_motif_in_seq_fistposition + 2 + len(post_motif_str) == len(seq):
         exist = False;
         return exist, frag1, frag2;
     else : 
@@ -51,12 +52,12 @@ def getfragments(seq, motif):
 
     #4
     #print(seq);
-    #print(seq[:pre_motif_in_seq_fistposition+1]);
-    #print(seq[pre_motif_in_seq_fistposition+1:]);
-    frag1 = seq[:post_motif_in_seq_fistposition+1+pre_motif_in_seq_fistposition+1];
-    frag2 = seq[post_motif_in_seq_fistposition+1+pre_motif_in_seq_fistposition+1:];
-    #print("frag1: ", frag1)
-    #print("frag2: ", frag2)
+    #print(seq[:pre_motif_in_seq_fistposition]);
+    #print(seq[pre_motif_in_seq_fistposition:]);
+    frag1 = seq[:post_motif_in_seq_fistposition+len(pre_motif_str)+pre_motif_in_seq_fistposition];
+    frag2 = seq[post_motif_in_seq_fistposition+len(pre_motif_str)+pre_motif_in_seq_fistposition:];
+    print("frag1: ", frag1)
+    print("frag2: ", frag2)
 
 
     
@@ -65,6 +66,8 @@ def getfragments(seq, motif):
 def main():
     seq = input("Give me the sequence: ")
     motif = input("Give me the enzyme motif: ")
+
+
 
 
     exist, frag1, frag2 = getfragments(seq, motif)
